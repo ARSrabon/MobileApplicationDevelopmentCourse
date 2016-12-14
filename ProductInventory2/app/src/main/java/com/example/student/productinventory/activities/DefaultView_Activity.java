b5282ea4f5787dac71707523abdcd81e775ea752
ARSrabon <m.arsrabon@gmail.com>
2016-12-14 13:06:38 +0600

    added material drawer to Productinventory2 and deleted productinventory project

--- ProductInventory2/app/src/main/java/com/example/student/productinventory/activities/DefaultView_Activity.java
+++ ProductInventory2/app/src/main/java/com/example/student/productinventory/activities/DefaultView_Activity.java
@@ -10,8 +10,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.activeandroid.query.Select;
@@ -20,10 +22,22 @@ import com.example.student.productinventory.adapters.ProductAdapter;
 import com.example.student.productinventory.models.Category;
 import com.example.student.productinventory.models.Product;
 import com.example.student.productinventory.models.User;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.materialdrawer.AccountHeader;
+import com.mikepenz.materialdrawer.AccountHeaderBuilder;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem;
+import com.mikepenz.materialdrawer.model.ProfileSettingDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IProfile;
 
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static android.R.attr.enabled;
+import static android.R.attr.icon;
 
 public class DefaultView_Activity extends AppCompatActivity {
 
@@ -32,18 +46,22 @@ public class DefaultView_Activity extends AppCompatActivity {
     ProductAdapter productAdapter;
     RecyclerView recyclerView;
 
+    User user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.defaultview_activity);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
 
         sharedPreferences = getSharedPreferences("Product_inventory_prefs", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
         if (sharedPreferences.getBoolean("loggedin", false)) {
-            User user = User.load(User.class,sharedPreferences.getLong("userId",-1));
-            Toast.makeText(DefaultView_Activity.this, "Welcome, " + user.getUser_name() + "  " + user.getUserType(), Toast.LENGTH_LONG).show();
+
+            user = User.load(User.class, sharedPreferences.getLong("userId", -1));
+//            Toast.makeText(DefaultView_Activity.this, "Welcome, " + user.getUser_name() + "  " + user.getUserType(), Toast.LENGTH_LONG).show();
         }
 
         try {
@@ -51,6 +69,81 @@ public class DefaultView_Activity extends AppCompatActivity {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        AccountHeader headerResult = new AccountHeaderBuilder()
+                .withActivity(this)
+                .withHeaderBackground(R.drawable.proback_a)
+                .addProfiles(
+                        new ProfileDrawerItem().withName(user.getUser_name())
+                                .withEmail(user.getUser_email())
+                                .withIcon(getResources().getDrawable(R.drawable.pexels_photo)))
+                .withOnAccountHeaderListener(new AccountHeader.OnAccountHeaderListener() {
+                    @Override
+                    public boolean onProfileChanged(View view, IProfile profile, boolean currentProfile) {
+                        return false;
+                    }
+                })
+                .build();
+
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withAccountHeader(headerResult)
+                .withToolbar(myToolbar)
+                .inflateMenu(R.menu.activity_main_actions)
+                .build();
+
+        result.getActionBarDrawerToggle().setDrawerIndicatorEnabled(true);
+
+        result.setOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+            @Override
+            public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+//                Toast.makeText(this, String.valueOf(position) ,Toast.LENGTH_LONG, drawerItem.getTag()).show();
+//        try {
+//            Thread.sleep(1000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+                Intent intent;
+                switch ((int) drawerItem.getIdentifier()) {
+                    case R.id.add_prouct:
+                        intent = new Intent(DefaultView_Activity.this, Add_New_Product_Activity.class);
+                        startActivity(intent);
+                        break;
+                    case R.id.stock_updater:
+                        intent = new Intent(DefaultView_Activity.this, Product_Stock_Updater_Activity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                    case R.id.sell_product:
+                        intent = new Intent(DefaultView_Activity.this, Product_Sell_Activity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+
+                    case R.id.add_category:
+                        break;
+
+                    case R.id.customerDues:
+                        intent = new Intent(DefaultView_Activity.this, CustomersDueActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+
+                    case R.id.sale_history:
+                        intent = new Intent(DefaultView_Activity.this, SellHistoryViewActivity.class);
+                        startActivity(intent);
+                        finish();
+                        break;
+                    case R.id.action_logout:
+                        SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.My_sharedPrefs_file_name), Context.MODE_PRIVATE).edit();
+                        editor.putBoolean("loggedin", false);
+                        editor.commit();
+                        finish();
+                        break;
+                }
+                return false;
+            }
+        });
 
         if (sharedPreferences.getString("dbempty", "true").equals("true")) {
             ArrayList<Category> category = new ArrayList<Category>();
@@ -129,7 +222,8 @@ public class DefaultView_Activity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
                 break;
-            case R.id.sell_product: intent = new Intent(DefaultView_Activity.this,Product_Sell_Activity.class);
+            case R.id.sell_product:
+                intent = new Intent(DefaultView_Activity.this, Product_Sell_Activity.class);
                 startActivity(intent);
                 finish();
                 break;
@@ -137,12 +231,14 @@ public class DefaultView_Activity extends AppCompatActivity {
             case R.id.add_category:
                 break;
 
-            case R.id.customerDues: intent = new Intent(DefaultView_Activity.this,CustomersDueActivity.class);
+            case R.id.customerDues:
+                intent = new Intent(DefaultView_Activity.this, CustomersDueActivity.class);
                 startActivity(intent);
                 finish();
                 break;
 
-            case R.id.sale_history: intent = new Intent(DefaultView_Activity.this,SellHistoryViewActivity.class);
+            case R.id.sale_history:
+                intent = new Intent(DefaultView_Activity.this, SellHistoryViewActivity.class);
                 startActivity(intent);
                 finish();
                 break;
